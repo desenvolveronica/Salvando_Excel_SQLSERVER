33f11a0f1b63b8bb75fdfa05fa2e6063ace05c39
Verônica Helena <desenvolveronica@gmail.com>
2023-10-02 17:58:37 -0300

    Conexão string StrCon

--- Salvando dados Excel no Banco de dados em CSharp/DataBase.cs
+++ Salvando dados Excel no Banco de dados em CSharp/DataBase.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Salvando_dados_Excel_no_Banco_de_dados_em_CSharp
 {
-    internal class DataBase
+    public class DataBase
     {
+        //Data Source = VERONICAPC\SQLSERVER1993;Initial Catalog = Livraria; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False
+
+        private static string server = @"VERONICAPC\SQLSERVER1993";
+        private static string dataBase = "Livraria";
+
+        public static string StrCon
+        {
+            get { return $"Data Source = {server};Initial Catalog = {dataBase}; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;ApplicationIntent = ReadWrite; MultiSubnetFailover=False";  }
+        }
+
     }
 }

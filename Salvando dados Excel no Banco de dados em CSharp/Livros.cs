fc90175cd26cac45e228f51cbddc6f0782ba89ec
Verônica Helena <desenvolveronica@gmail.com>
2023-10-02 17:40:59 -0300

    Propriedades e construtores

--- Salvando dados Excel no Banco de dados em CSharp/Livros.cs
+++ Salvando dados Excel no Banco de dados em CSharp/Livros.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Salvando_dados_Excel_no_Banco_de_dados_em_CSharp
 {
-    internal class Livros
+    public class Livros
     {
+        public string Titulo { get; set; }
+        public string Isbn { get; set; }
+        public string Autores { get; set; }
+        public string Assuntos { get; set; }
+        public decimal Unitario { get; set; }
+
+        public Livros() { }//construtor padrão
+
+        public Livros(string titulo, string isbn, string autores, string assunto, decimal unitario)//construtor
+        {//propriedades = parâmetros
+            Titulo = titulo;
+            Isbn = isbn;
+            Autores = autores;
+            Assuntos = assunto;
+            Unitario = unitario;
+
+        }
+
+
+
     }
 }
